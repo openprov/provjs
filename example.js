0273ad1f8002a6e3dff6200464384d3676dcdd48
Trung Dong Huynh <trungdong@donggiang.com>
2013-12-10 00:56:18 +0000

    Registered "prov" namespace by default

--- example.js
+++ example.js
@@ -13,20 +13,20 @@ var ex = prov.addNamespace("ex", "http://www.example.org/");
 var e1 = prov.entity("ex:e1");
 e1.set_attr(ex.qname("foo"), ex.qname("bar"));
 console.log(e1);
-console.log(""+e1);
+console.log("" + e1);
 
 var der1 = prov.wasDerivedFrom("ex:e2", "ex:e1");
-der1.set_attr(prov.ns.qname("type"), prov.ns.qname("Revision"));
-der1.id = ex.qname('d1');
-der1.activity=ex.qname('a1');
-der1.set_attr(prov.ns.qname("type"), prov.ns.qname("Revision"));
+der1.set_attr("prov:type", "prov:Revision");
+der1.id(ex.qname('d1'));
+der1.activity = ex.qname('a1');
+der1.set_attr("prov:type", "prov:Revision");
 console.log(der1);
-console.log(""+der1);
+console.log("" + der1);
 
 console.log(der1.get_attr(prov.ns.qname("type")));
 
-var der2 = prov.wasDerivedFrom("ex:e2", "ex:e1", prov.ns.qname("type"), prov.ns.qname("Revision"));
+var der2 = prov.wasDerivedFrom("ex:e2", "ex:e1", "prov:type", "prov:Revision");
 console.log(der2);
-console.log(""+der2);
+console.log("" + der2);
 
 
--- prov.js
+++ prov.js
@@ -59,6 +59,10 @@ function Literal(value, datatype, langtag) {
 function Record() {
 	this.attributes = [];
 }
+Record.prototype.id = function(identifier) {
+	this.identifier = identifier;
+	return this;
+};
 Record.prototype.get_attr = function(k)
 {
 	var result = [];
@@ -75,22 +79,22 @@ Record.prototype.set_attr = function(k, v){
 };
 
 // Element
-function Element(id) {
+function Element(identifier) {
 	Record.call(this);
-	this.id = id;
+	this.identifier = identifier;
 };
 Element.prototype = new Record;
 Element.prototype.constructor = Element;
 
 // Entity
-function Entity(id) {
-	Element.call(this, id);
+function Entity(identifier) {
+	Element.call(this, identifier);
 };
 Entity.prototype = new Element;
 Entity.prototype.constructor = Entity;
 Entity.prototype.toString = function() {
 	var output = [];
-	output.push("" + this.id);
+	output.push("" + this.identifier);
 	var attr = this.attributes.map(function(x) {
 		return x.join("=");
 		}).join(", ");
@@ -122,18 +126,24 @@ Relation.prototype.constructor = Relation;
 Relation.prototype.toString = function() {
 	var that = this;
 	var output = [];
-	if (this.id) {
-		output.push(""+this.id + ";" + this[this.from]);
+	if (this.identifier) {
+		output.push("" + this.identifier + "; " + this[this.from]);
 	} else {
 		output.push(this[this.from]);
 	}
 	output.push(this[this.to]);
-	var rel = this.relations.map(function(x){return (that[x])?that[x]:"";}).join(", ");
-	if (rel.split(", ").join("")!=="") {
+	var rel = this.relations.map(function(x) {
+		return (that[x]) ? that[x] : "";
+	}).join(", ");
+	if (rel.split(", ").join("") !== "") {
 		output.push(rel);
 	}
-	var attr = this.attributes.map(function(x){return x.join("=");}).join(", ");
-	if (attr!=="") { output.push("["+attr+"]"); }
+	var attr = this.attributes.map(function(x) {
+		return x.join("=");
+	}).join(", ");
+	if (attr !== "") {
+		output.push("["+attr+"]");
+	}
 	return this.relation_name + "(" + output.join(", ") + ")";
 };
 
@@ -179,7 +189,9 @@ function ProvJS() {
 	// Keeping track of namespaces
 	// Convenient methods for generating PROV statements
 	// Construct a document
-	this.namespaces = {};
+	this.namespaces = {
+		"prov": this.ns,
+	};
 };
 ProvJS.prototype.addNamespace = function(ns_or_prefix, uri) {
 	var ns;
@@ -190,12 +202,12 @@ ProvJS.prototype.addNamespace = function(ns_or_prefix, uri) {
 	this.namespaces[ns.prefix] = ns;
 	return ns;
 };
-ProvJS.prototype.getValidQualifiedName = function(id) {
-	if (id instanceof QualifiedName)
-		return id;
+ProvJS.prototype.getValidQualifiedName = function(identifier) {
+	if (identifier instanceof QualifiedName)
+		return identifier;
 
 	// If id_str has a colon (:), check if the part before the colon is a registered prefix
-	var components = id.split(":", 2);
+	var components = identifier.split(":", 2);
 	if (components.length == 2) {
 		var prefix = components[0];
 		if (prefix in this.namespaces)
@@ -204,25 +216,25 @@ ProvJS.prototype.getValidQualifiedName = function(id) {
 		
 	// TODO If a default namespace is registered, use it
 
-	console.error("Cannot validate identifier:", id);
-	return id;
+	console.error("Cannot validate identifier:", identifier);
+	return identifier;
 };
-ProvJS.prototype.entity = function(id, attr_value_pairs) {
-	return new Entity(this.getValidQualifiedName(id), attr_value_pairs);
+ProvJS.prototype.entity = function(identifier) {
+	return new Entity(this.getValidQualifiedName(identifier));
 };
 ProvJS.prototype.wasDerivedFrom = function() {
 	var result;
 	var l = arguments.length;
-	if (l<2) {
+	if (l < 2) {
 		return null;
 	}
 	result = new Derivation(this.getValidQualifiedName(arguments[0]), this.getValidQualifiedName(arguments[1]));
-	if (l>2) {
-		for(var pos=3; pos<l; pos+=2) {
-			result.set_attr(this.getValidQualifiedName(arguments[pos-1]), arguments[pos]);
+	if (l > 2) {
+		for (var pos = 3; pos < l; pos += 2) {
+			result.set_attr(this.getValidQualifiedName(arguments[pos - 1]), arguments[pos]);
 		}
 	}
-	return(result);
+	return result;
 };
 ProvJS.prototype.ns = new Namespace("prov", "http://www.w3.org/ns/prov#");
 
