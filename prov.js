709da8df033bbbfd12bbbaba95157b3ce893894f
Danius Michaelides <dtm@ecs.soton.ac.uk>
2013-12-11 10:51:16 +0000

    Make JSLint abit happier.

--- prov.js
+++ prov.js
@@ -11,7 +11,7 @@
 // URI
 function URI(uri) {
 	this.uri = uri;
-};
+}
 
 URI.prototype.getURI = function() {
 	return this.uri;
@@ -24,7 +24,7 @@ function QualifiedName(prefix, localPart, namespaceURI) {
 	this.localPart = localPart;
 	this.namespaceURI = namespaceURI;
 	URI.call(this, namespaceURI + localPart);
-};
+}
 
 QualifiedName.prototype = Object.create(URI.prototype);
 QualifiedName.prototype.constructor = QualifiedName;
@@ -41,7 +41,7 @@ QualifiedName.prototype.equals = function(other) {
 function Namespace(prefix, namespaceURI) {
 	this.prefix = prefix;
 	this.namespaceURI = namespaceURI;
-};
+}
 
 Namespace.prototype.qname = function(localPart) {
 	var ret = new QualifiedName(this.prefix, localPart, this.namespaceURI);
@@ -80,7 +80,8 @@ Record.prototype = {
 	},
 	get_attr: function(attr_name) {
 		var results = [];
-		for (var i = 0; i < this.attributes.length; i++) {
+		var i;
+		for (i = 0; i < this.attributes.length; i++) {
 			if (attr_name.equals(this.attributes[i][0])) {
 				results.push(this.attributes[i][1]);
 			}
@@ -96,19 +97,19 @@ Record.prototype = {
 function Element(identifier) {
 	Record.call(this);
 	this.identifier = identifier;
-};
+}
 Element.prototype = Object.create(Record.prototype);
 Element.prototype.constructor = Element;
 
 // Entity
 function Entity(identifier) {
 	Element.call(this, identifier);
-};
+}
 Entity.prototype = Object.create(Element.prototype);
 Entity.prototype.constructor = Entity;
 Entity.prototype.toString = function() {
 	var output = [];
-	output.push("" + this.identifier);
+	output.push(String(this.identifier));
 	var attr = this.attributes.map(function(x) {
 		return x.join("=");
 		}).join(", ");
@@ -130,9 +131,10 @@ Entity.prototype.toString = function() {
 // Relation
 function Relation()
 {
+	var i;
 	Record.call(this);
-	for(r in this.relations) {
-		this[this.relations[r]] = null;
+	for(i=0; i<this.relations.length; i++) {
+		this[this.relations[i]] = null;
 	}
 }
 Relation.prototype = Object.create(Record.prototype);
@@ -141,7 +143,7 @@ Relation.prototype.toString = function() {
 	var that = this;
 	var output = [];
 	if (this.identifier) {
-		output.push("" + this.identifier + "; " + this[this.from]);
+		output.push(String(this.identifier) + "; " + this[this.from]);
 	} else {
 		output.push(this[this.from]);
 	}
@@ -202,7 +204,7 @@ var xsdNS = new Namespace("xsd", "http://www.w3.org/2000/10/XMLSchema#");
 
 // Utility class
 function ProvJS() {
-};
+}
 
 ProvJS.prototype = {
 	namespaces: {
@@ -214,23 +216,26 @@ ProvJS.prototype = {
 	constructor: ProvJS,
 	addNamespace: function(ns_or_prefix, uri) {
 		var ns;
-		if (ns_or_prefix instanceof Namespace)
+		if (ns_or_prefix instanceof Namespace) {
 			ns = ns_or_prefix;
-		else 
+		} else {
 			ns = new Namespace(ns_or_prefix, uri);
+		}
 		this.namespaces[ns.prefix] = ns;
 		return ns;
 	},
 	getValidQualifiedName: function(identifier) {
-		if (identifier instanceof QualifiedName)
+		if (identifier instanceof QualifiedName) {
 			return identifier;
+		}
 
 		// If id_str has a colon (:), check if the part before the colon is a registered prefix
 		var components = identifier.split(":", 2);
-		if (components.length == 2) {
+		if (components.length === 2) {
 			var prefix = components[0];
-			if (prefix in this.namespaces)
+			if (prefix in this.namespaces) {
 				return this.namespaces[prefix].qname(components[1]);
+			}
 		}
 			
 		// TODO If a default namespace is registered, use it
@@ -239,7 +244,7 @@ ProvJS.prototype = {
 		return identifier;
 	},
 	literal: function(value, datatype, langtag) {
-		if ((datatype===undefined) && (langtag==undefined)) {
+		if ((datatype===undefined) && (langtag===undefined)) {
 			if (typeof value === "string") {
 				datatype = xsdNS.qname("string");
 			} else
@@ -270,14 +275,14 @@ ProvJS.prototype = {
 		return new Entity(this.getValidQualifiedName(identifier));
 	},
 	wasDerivedFrom: function() {
-		var result;
+		var pos,result;
 		var l = arguments.length;
 		if (l < 2) {
 			return null;
 		}
 		result = new Derivation(this.getValidQualifiedName(arguments[0]), this.getValidQualifiedName(arguments[1]));
 		if (l > 2) {
-			for (var pos = 3; pos < l; pos += 2) {
+			for (pos = 3; pos < l; pos += 2) {
 				result.set_attr(this.getValidQualifiedName(arguments[pos - 1]), arguments[pos]);
 			}
 		}
